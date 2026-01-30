4f914b38dea17dee7a010d09aab60efb5b9466ef
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-01-31 01:23:45 +0530

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
+require("dotenv").config();  
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+
 const app = express();
-
-const PORT = process.env.PORT || 5000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("HEllo World");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running : ${PORT}`);
