3771f14013b40997e4a31a59802a7d2aaa50de24
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-01-31 15:10:16 +0530

    user controllers & routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const User = require("../model/userSchema");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, phone, role, addresses } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      role,
+      addresses,
+    });
+    res
+      .status(201)
+      .json({ message: "User registered successfully", userId: user._id });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.json({ message: "Login successful", token });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).select("-password");
+
+    res.status(200).json({ message: "users fetched successfully", users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getUsersById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id).select("-password");
+    res.status(201).json({ message: "user fetched successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
 
 connectDB();
 
 const app = express();
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.send("HEllo World");
-});
+app.use('/auth',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
