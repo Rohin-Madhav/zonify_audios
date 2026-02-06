301de721fca43999c2ced4e11908f11fc0ff0844
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-02-06 19:40:46 +0530

    product name changed

--- backend/models/orderSchema.js
+++ backend/models/orderSchema.js
@@ -14,7 +14,7 @@ const orderSchema = new mongoose.Schema(
           ref: "Product",
           required: true,
         },
-        name: { type: String, required: true },
+        productName: { type: String, required: true },
         price: { type: Number, required: true },
         quantity: { type: Number, required: true, min: 1 },
       },
