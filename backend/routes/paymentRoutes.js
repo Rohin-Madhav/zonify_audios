c5d4c886ee513240d4839cf1aa5faf7c105c8042
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-02-23 22:07:46 +0530

    payment routes

---
+++ backend/routes/paymentRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const paymentController = require("../controllers/paymentController");
+const auth = require("../middilwares/auth");
+const authorizeRoles = require("../middilwares/role");
+
+router.post("/initialize", auth, paymentController.initializeRazorpayPayment);
+router.post("/webhook", paymentController.handleRazorpayWebhook);
+router.post("/retry", auth, paymentController.retryPayment);
+router.post("/refund",auth,authorizeRoles("admin"),paymentController.refundPayment)
+router.get("/all",auth,authorizeRoles("admin"),paymentController.getAllPayments)
+
+module.exports = router;
